8114ff7b99a542dd84ec71d747ca6feb26293781
Yashas Majmudar <64674824+yashas-hm@users.noreply.github.com>
2024-04-01 09:47:36 +0530

    Delete src directory

--- src/app/src/main/java/com/yashas/chequescanner/database/ScanHistoryDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.yashas.chequescanner.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ScanHistoryEntity::class], version = 1)
-abstract class ScanHistoryDatabase: RoomDatabase() {
-    abstract fun scanHistoryDao(): ScanHistoryDao
-}
